e3d96b31c4c4dce92786bcf96105e37b1573ae6f
makoto-kubodera <makoto-kubodera@linked-ideal.com>
2021-10-08 21:14:37 +0900

    first commit

---
+++ src/main/scala/com/ideal/linked/toposoid/common/ToposoidUtils.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 Linked Ideal LLC.[https://linked-ideal.com/]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ideal.linked.toposoid.common
+
+/**
+ * Common Utilities in all toposoid project.
+ */
+object ToposoidUtils {
+
+  /**
+   * Returns the Neo4J node type that corresponds to the sentence type.
+   * @param sentenceType
+   * @return
+   */
+  def getNodeType(sentenceType:Int): String ={
+    val nodeType:String = sentenceType match{
+      case PREMISE.index => "PremiseNode"
+      case CLAIM.index => "ClaimNode"
+      case _ => "UnknownNode"
+    }
+    nodeType
+  }
+
+}
