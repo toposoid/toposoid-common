7917b251805214374a1a3d3b579e863fb55624a4
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2024-08-25 08:34:12 +0900

    https://github.com/toposoid/toposoid-common/issues/59

--- src/main/scala/com/ideal/linked/toposoid/common/TransversalState.scala
+++ src/main/scala/com/ideal/linked/toposoid/common/TransversalState.scala
@@ -18,9 +18,11 @@ package com.ideal.linked.toposoid.common
 
 import play.api.libs.json.{Json, OWrites, Reads}
 
-case class TransversalState(username:String)
+case class TransversalState(userId: String, roleId: Int, username: String, csrfToken: String)
 object TransversalState {
   implicit val jsonWrites: OWrites[TransversalState] = Json.writes[TransversalState]
   implicit val jsonReads: Reads[TransversalState] = Json.reads[TransversalState]
 }
+
+
 
