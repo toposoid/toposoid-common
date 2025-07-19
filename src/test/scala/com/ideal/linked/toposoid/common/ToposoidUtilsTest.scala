034d7e1f98978287d8df7cf374616571ae31eb03
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2025-07-19 14:24:01 +0900

    src/test/scala/com/ideal/linked/toposoid/common/ToposoidUtilsTest.scala

--- src/test/scala/com/ideal/linked/toposoid/common/ToposoidUtilsTest.scala
+++ src/test/scala/com/ideal/linked/toposoid/common/ToposoidUtilsTest.scala
@@ -29,31 +29,4 @@ class ToposoidUtilsTest extends AnyFlatSpec with BeforeAndAfter with BeforeAndAf
     assert(result.contains("\"data\": \"テスト\""))
   }
 
-  /*
-  "request to sentence-parser-japanese" should "be handled properly" in {
-
-    val jsonStr:String = """
-                           |  {
-                           |    "premise": [],
-                           |    "claim": [
-                           |      {
-                           |        "propositionId": "612bf3d6-bdb5-47b9-a3a6-185015c8c414",
-                           |        "sentenceId": "4a2994a1-ec7a-438b-a290-0cfb563a5170",
-                           |        "knowledge": {
-                           |          "sentence": "案ずるより産むが易し。",
-                           |          "lang": "ja_JP",
-                           |          "extentInfoJson": "{}",
-                           |          "isNegativeSentence": false,
-                           |          "knowledgeForImages": []
-                           |        }
-                           |      }
-                           |    ]
-                           |  }
-                           |""".stripMargin
-
-
-    val result: String = ToposoidUtils.callComponent(jsonStr, "192.168.33.10", "9001", "analyze", transversalState)
-    print(result)
-  }
-  */
 }
