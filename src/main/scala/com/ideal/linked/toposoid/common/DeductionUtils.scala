56055f6cf9a6aac56b3c5cc2144627145b8748b7
makoto-kubodera <makoto.kubodera@linked-ideal.com>
2026-04-28 00:30:35 +0000

    https://github.com/toposoid/toposoid-common/issues/131

--- src/main/scala/com/ideal/linked/toposoid/common/DeductionUtils.scala
+++ src/main/scala/com/ideal/linked/toposoid/common/DeductionUtils.scala
@@ -31,6 +31,14 @@ import com.ideal.linked.toposoid.protocol.model.base.CoveredPropositionEdge
 import com.ideal.linked.toposoid.protocol.model.neo4j.Neo4jRecords
 import com.ideal.linked.toposoid.knowledgebase.model.KnowledgeBaseSynonymNode
 import com.ideal.linked.toposoid.knowledgebase.model.KnowledgeFeatureReference
+import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import play.api.libs.json.{Json, __}
+
+
+case class DeductionQuery(query:String,relationMatchState:RelationMatchState, sourceAlias:String, destinationAlias:String,isSourceConfirmed:Boolean, isDestinationConfirmed:Boolean, featureSimilarityMap:Map[String, Float] = Map.empty[String, Float])
 
 object DeductionUtils extends LazyLogging {
 
@@ -51,7 +59,118 @@ object DeductionUtils extends LazyLogging {
         })    
     }
 
-    def getCoveredPropositionEdge(edge: KnowledgeBaseEdge, sourceAlias:String, destinationAlias:String, nodeMap:Map[String, KnowledgeBaseNode], neo4jRecords: Neo4jRecords, relationMatchState:RelationMatchState, deductionUnitName:String, featureSimilarityMap:Map[String, Float] = Map.empty[String, Float]):CoveredPropositionEdge = {
+    def analyzeGraphKnowledge(getQeuries:(KnowledgeBaseEdge, Map[String, KnowledgeBaseNode], TransversalState) => List[DeductionQuery], edges: List[KnowledgeBaseEdge], aso:AnalyzedSentenceObject, transversalState:TransversalState):List[CoveredPropositionEdge] = {    
+        val futures: List[Future[Option[CoveredPropositionEdge]]] = edges.foldLeft(List.empty[Future[Option[CoveredPropositionEdge]]]){
+        (acc, edge) => {
+            val deductionQueries = getQeuries(edge, aso.nodeMap, transversalState)       
+            deductionQueries.size match {
+            case 0 => acc :+ Future(Option(aso.deductionResult.coveredPropositionEdges.filter(x => x.sourceNode.terminalId.equals(edge.sourceId) && x.destinationNode.terminalId.equals(edge.destinationId)).head))
+            case _ => acc :+ Future(analyzeEdge(0, deductionQueries, edge, aso, Neo4JUtilsImpl(), transversalState))
+            }        
+        }
+        }    
+        val combinedFuture: Future[List[Option[CoveredPropositionEdge]]] = Future.sequence(futures)
+        val result = Await.result(combinedFuture, Duration.Inf)    
+        result.flatten
+    }
+
+    private def analyzeEdge(idx:Int, deductionQueries:List[DeductionQuery],edge:KnowledgeBaseEdge, aso:AnalyzedSentenceObject, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState):Option[CoveredPropositionEdge] = {
+        val nodeMap = aso.nodeMap
+        val sourceNode = nodeMap.get(edge.sourceId).get.asInstanceOf[KnowledgeBaseNode]
+        val destinationNode = nodeMap.get(edge.destinationId).get.asInstanceOf[KnowledgeBaseNode]
+
+        //引数のisSourceConfirmed, isDestinationConfirmedとdeductionQueries(idx)のisSourceConfirmed, isDestinationConfirmedが同じかをチェックする。
+        //チェックNGの場合は、analyzeEdge(idx+1, deductionQueries, edge, nodeMap, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)    
+        val coveredPropositionEdges = aso.deductionResult.coveredPropositionEdges.filter(x => {
+            x.sourceNode.terminalId.equals(edge.sourceId) && x.destinationNode.terminalId.equals(edge.destinationId)
+        })
+        val (isSourceConfirmed, isDestinationConfirmed, coveredPropositionEdge) = coveredPropositionEdges.size match {
+            case 0 => (false, false, None) //BaseMatch用
+            case _ => (coveredPropositionEdges.head.sourceNode.isConfirmed, coveredPropositionEdges.head.destinationNode.isConfirmed, Option(coveredPropositionEdges.head))
+        }
+        //クエリを実行する必要のない場合は、早めに判断し次のクエリを実行を促す。
+        if(!deductionQueries(idx).isSourceConfirmed == isSourceConfirmed || !deductionQueries(idx).isDestinationConfirmed == isDestinationConfirmed){
+            if(idx + 1 < deductionQueries.size) analyzeEdge(idx+1, deductionQueries, edge, aso, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)
+            else coveredPropositionEdge  
+        }else{
+
+            val sourceMorphemes = sourceNode.predicateArgumentStructure.morphemes
+            val destinationMorphemes = destinationNode.predicateArgumentStructure.morphemes
+            val isVerbOrNounOnSource = sourceNode.localContext.lang match {
+            case "ja_JP" =>  sourceMorphemes.filter(x => x.split(",").toList.contains("動詞")).size > 0 || sourceMorphemes.filter(x => x.split(",").toList.contains("名詞")).size > 0
+            case "en_US" => sourceMorphemes.filter(x => x.split(",").toList.contains("VERB")).size > 0  || sourceMorphemes.filter(x => x.split(",").toList.contains("NOUN")).size > 0
+            }
+            val isVerbOrNounOnDestination = destinationNode.localContext.lang match {
+            case "ja_JP" =>  destinationMorphemes.filter(x => x.split(",").toList.contains("動詞")).size > 0 || destinationMorphemes.filter(x => x.split(",").toList.contains("名詞")).size > 0
+            case "en_US" => destinationMorphemes.filter(x => x.split(",").toList.contains("VERB")).size > 0  || destinationMorphemes.filter(x => x.split(",").toList.contains("NOUN")).size > 0
+            }
+
+            deductionQueries(idx).relationMatchState match {
+            case RelationMatchState.MATCHED_BOTH => {        
+                analyze(idx, deductionQueries, edge, nodeMap, neo4JUtils, transversalState) match {
+                case Some(x) => Option(x)
+                case _ => {
+                    if(idx + 1 < deductionQueries.size) analyzeEdge(idx+1, deductionQueries, edge, aso, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)
+                    else coveredPropositionEdge
+                }}
+            }
+            case RelationMatchState.MATCHED_SOURCE_NODE_ONLY => {
+                if(isVerbOrNounOnDestination){
+                analyze(idx, deductionQueries, edge, nodeMap, neo4JUtils, transversalState) match {
+                    case Some(x) => Option(x)
+                    case _ => {
+                    if(idx + 1 < deductionQueries.size) analyzeEdge(idx+1, deductionQueries, edge, aso, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)
+                    else coveredPropositionEdge  
+                    }}          
+                }else {
+                if(idx + 1 < deductionQueries.size) analyzeEdge(idx+1, deductionQueries, edge, aso, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)
+                else coveredPropositionEdge        
+                }
+            }
+            case RelationMatchState.MATCHED_TARGET_NODE_ONLY => {
+                if(isVerbOrNounOnSource) {
+                analyze(idx, deductionQueries, edge, nodeMap, neo4JUtils, transversalState) match {
+                    case Some(x) => Option(x)
+                    case _ => {
+                    if(idx + 1 < deductionQueries.size) analyzeEdge(idx+1, deductionQueries, edge, aso, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)
+                    else coveredPropositionEdge  
+                    }}          
+                }else {
+                if(idx + 1 < deductionQueries.size) analyzeEdge(idx+1, deductionQueries, edge, aso, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)
+                else coveredPropositionEdge        
+                }
+            }
+            case RelationMatchState.NOT_MATCHED_BOTH => {
+                if(isVerbOrNounOnSource && isVerbOrNounOnDestination){
+                analyze(idx, deductionQueries, edge, nodeMap, neo4JUtils, transversalState) match {
+                    case Some(x) => Option(x)
+                    case _ => {
+                    if(idx + 1 < deductionQueries.size) analyzeEdge(idx+1, deductionQueries, edge, aso, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)
+                    else coveredPropositionEdge  
+                    }}          
+                }else {
+                if(idx + 1 < deductionQueries.size) analyzeEdge(idx+1, deductionQueries, edge, aso, neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState)
+                else coveredPropositionEdge        
+                }
+            }
+            }
+        }
+    }
+  
+    private def analyze(idx:Int, deductionQueries:List[DeductionQuery],edge:KnowledgeBaseEdge, nodeMap: Map[String, KnowledgeBaseNode], neo4JUtils:Neo4JUtilsImpl, transversalState:TransversalState):Option[CoveredPropositionEdge] = {
+        val deductionUnitName = conf.getString("TOPOSOID_DEDUCTION_UNIT_NAME")
+        val jsonStr: String = neo4JUtils.getCypherQueryResult(deductionQueries(idx).query, "", transversalState)
+        //If there is even one that does not match, it is useless to search further
+        if (!jsonStr.equals("""{"records":[]}""")) {
+        //ヒットするものがある場合
+        val neo4jRecords: Neo4jRecords = Json.parse(jsonStr).as[Neo4jRecords]      
+        Option(DeductionUtils.getCoveredPropositionEdge(edge, deductionQueries(idx).sourceAlias, deductionQueries(idx).destinationAlias, nodeMap,  neo4jRecords, deductionQueries(idx).relationMatchState, deductionUnitName, deductionQueries(idx).featureSimilarityMap))        
+        }else{
+        None
+        }
+    }
+
+    private def getCoveredPropositionEdge(edge: KnowledgeBaseEdge, sourceAlias:String, destinationAlias:String, nodeMap:Map[String, KnowledgeBaseNode], neo4jRecords: Neo4jRecords, relationMatchState:RelationMatchState, deductionUnitName:String, featureSimilarityMap:Map[String, Float] = Map.empty[String, Float]):CoveredPropositionEdge = {
         //一旦どちらかのノードが埋まっていれば推論を進めるものとする。        
         val (isConfirmedSource, isConfirmedDestination)= relationMatchState match {
             case RelationMatchState.MATCHED_BOTH => (true, true)
@@ -84,7 +203,7 @@ object DeductionUtils extends LazyLogging {
         CoveredPropositionEdge(sourceNode = sourceNode, destinationNode = destinationNode)
     }
 
-    def getMatchedKnowledgeNodes(
+    private def getMatchedKnowledgeNodes(
         edge: KnowledgeBaseEdge, 
         serchedKnowledgeNodes:List[KnowledgeBaseNode | KnowledgeBaseSynonymNode | KnowledgeFeatureReference], 
         proopsitionNode: KnowledgeBaseNode,
